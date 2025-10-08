ab87f11f8eef1ec9ea28ad7329b6971e8ca13abb
cablandino <carlos@cbonlinemedia.com>
2025-10-07 21:33:39 -0700

    track prev and next current index functionality

--- assets/quiz.js
+++ assets/quiz.js
@@ -1,11 +1,15 @@
 const finish_btn = document.getElementById("finish-button");
+const prev_button = document.getElementById("prev-button");
+const next_button = document.getElementById("next-button");
 const quiz_form = document.querySelector("#quizForm");
+let currentIndex = 0;
 const answers = {};
 
 quiz_form.addEventListener("change", updateAnswers);
 function updateAnswers(event) {
   //code to store answers
-  answers[event.target.name] = event.target.value;
+
+  console.log("answers: ", answers);
 }
 
 finish_btn.addEventListener("click", function () {
@@ -13,5 +17,15 @@ finish_btn.addEventListener("click", function () {
     alert("Here are your answers: " + JSON.stringify(answers));
   } else {
     alert("Please answer all the questions!");
+  }
+});
+next_button.addEventListener("click", function () {
+  if (currentIndex < questions.length - 1) {
+    currentIndex++;
+  }
+});
+prev_button.addEventListener("click", function () {
+  if (currentIndex > 0) {
+    currentIndex--;
   }
 });
